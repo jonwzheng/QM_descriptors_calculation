ef5f295a7b7a0d05078f827902ac200ae5d2d0a9
Hao-Wei Pang <hwpang@mit.edu>
2022-06-27 18:37:08 -0400

    rename

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,7 +1,7 @@
 from .dft_calculation import dft_scf_qm_descriptor, dft_scf_opt, dft_scf_sp, save_dft_sp_results
 from .file_parser import mol2xyz, xyz2com
 from .log_parser import G16Log, XtbLog
-from .ff_conf_generation import csearch
+from .FF_conf_generation import csearch
 from .grab_QM_descriptors import read_log
 from .utils import create_logger, DoneJobsRecord
 from .semiempirical_calculation import semiempirical_opt, xtb_status
