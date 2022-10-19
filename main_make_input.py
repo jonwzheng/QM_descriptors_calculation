c5262a3d2b97d4bae93dffb23fa3cdef286366c3
Hao-Wei Pang <hwpang@mit.edu>
2022-10-19 17:21:31 -0400

    make input

---
+++ main_make_input.py
@@ -0,0 +1,69 @@
+from argparse import ArgumentParser
+import os
+import shutil
+import time
+import tarfile
+import csv
+
+import pickle as pkl
+import pandas as pd
+import traceback
+
+import rdkit.Chem as Chem
+
+from lib.wft_calculation import generate_dlpno_sp_input
+
+parser = ArgumentParser()
+parser.add_argument('--output_folder', type=str, default='output',
+                    help='output folder name')
+parser.add_argument('--xyz_DFT_opt', type=str, default=None,
+                    help='pickle file containing a dictionary to map between the mol_id and DFT-optimized xyz for following calculations',)
+parser.add_argument('--DLPNO_sp_folder', type=str, default='DLPNO_sp')
+parser.add_argument('--DLPNO_sp_n_procs', type=int, default=22,
+                    help='number of process for DLPNO calculations')
+parser.add_argument('--DLPNO_sp_job_ram', type=int, default=3900,
+                    help='amount of ram (MB) per core allocated for each DLPNO calculation')
+
+args = parser.parse_args()
+
+# input files
+with open(args.xyz_DFT_opt, "rb") as f:
+    xyz_DFT_opt = pkl.load(f)
+
+df = pd.read_csv(args.input_smiles, index_col=0)
+
+# create id to smile mapping
+mol_id_to_smi_dict = dict(zip(df.id, df.smiles))
+mol_id_to_charge_dict = dict()
+mol_id_to_mult_dict = dict()
+for k, v in mol_id_to_smi_dict.items():
+    try:
+        mol = Chem.MolFromSmiles(v)
+    except Exception as e:
+        print(f'Cannot translate smi {v} to molecule for species {k}')
+
+    try:
+        charge = Chem.GetFormalCharge(mol)
+        mol_id_to_charge_dict[k] = charge
+    except Exception as e:
+        print(f'Cannot determine molecular charge for species {k} with smi {v}')
+
+    num_radical_elec = 0
+    for atom in mol.GetAtoms():
+        num_radical_elec += atom.GetNumRadicalElectrons()
+    mol_id_to_mult_dict[k] =  num_radical_elec + 1
+
+submit_dir = os.path.abspath(os.getcwd())
+project_dir = os.path.abspath(os.path.join(args.output_folder))
+
+os.makedirs(os.path.join(project_dir, args.DLPNO_sp_folder), exist_ok=True)
+mol_ids = list(xyz_DFT_opt.keys())
+for mol_id in mol_ids:
+    charge = mol_id_to_charge_dict[mol_id]
+    mult = mol_id_to_mult_dict[mol_id]
+    coords = xyz_DFT_opt[mol_id]
+    script = generate_dlpno_sp_input(coords, charge, mult, args.DLPNO_sp_n_procs, args.DLPNO_sp_job_ram)
+
+    infile = f"{mol_id}.in"
+    with open(os.path.join(project_dir, args.DLPNO_sp_folder, infile), "w+") as f:
+        f.write(script)
