624c38bfad97bc25925494a04b623b12b802bb75
Hao-Wei Pang <hwpang@mit.edu>
2022-10-30 18:38:42 -0400

    don't import removed functions

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,7 +1,6 @@
 from .dft_calculation import dft_scf_qm_descriptor, dft_scf_opt, dft_scf_sp, save_dft_sp_results
 from .file_parser import mol2xyz, xyz2com
 from .log_parser import G16Log, XtbLog
-from .FF_conf_generation import csearch
 from .grab_QM_descriptors import read_log
 from .utils import create_logger, DoneJobsRecord, REPLACE_LETTER
 from .semiempirical_calculation import semiempirical_opt, xtb_status
