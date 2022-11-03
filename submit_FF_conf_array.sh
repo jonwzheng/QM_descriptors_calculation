3cd4f80c713584c827d38bd0aec8b46b592d3bf3
Hao-Wei Pang <hwpang@mit.edu>
2022-11-03 15:52:40 -0400

    submit script for FF conf

--- submit_FF_conf_array.sh
+++ submit_FF_conf_array.sh
@@ -1,7 +1,4 @@
 #!/bin/bash -l
-#SBATCH -J FF_input
-#SBATCH -n 1
-#SBATCH --array=0-31
 
 echo "============================================================"
 echo "Job ID : $SLURM_JOB_ID"
@@ -11,7 +8,12 @@ echo "Running on node : $SLURMD_NODENAME"
 echo "Current directory : $(pwd)"
 echo "============================================================"
 
-conda activate rdmc_env
+# conda activate rdmc_env
+
+#RDMC for gaussian-xtb
+RDMC_PATH=/home/gridsan/groups/RMG/Software/RDMC-main
+export PATH=$RDMC_PATH:$PATH
+export PYTHONPATH=$PYTHONPATH:$RDMC_PATH
 
 #xtb
 source /home/gridsan/groups/RMG/Software/xtb-6.4.1/share/xtb/config_env.bash
@@ -20,9 +22,11 @@ export PATH=$XTB_PATH:$PATH
 
 #QMD
 QMD_PATH=/home/gridsan/groups/RMG/Software/QM_descriptors_calculation-radical_workflow
-input_smiles=/home/gridsan/groups/RMG/Projects/Hao-Wei-Oscar-Yunsie/production_run/HAbs/inputs/TS_cosmo_dlpno/wb97xd_and_xtb_opted_ts_combo_results_hashed_sep1a_ts_input.csv
+input_smiles=reactants_products_wb97xd_and_xtb_opted_ts_combo_results_hashed_chart_aug11b.csv
 
-scratch_dir=$HOME/scratch/$SLURM_JOB_ID-$SLURM_ARRAY_TASK_ID
+scratch_dir=$TMPDIR/$USER/$SLURM_JOB_ID-$SLURM_ARRAY_TASK_ID
 mkdir -p $scratch_dir
 
 python $QMD_PATH/main_FF_conf.py --input_smiles $input_smiles --XTB_path $XTB_PATH --scratch_dir $scratch_dir
+
+rm -rf $scratch_dir
