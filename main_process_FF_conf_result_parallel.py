3507b47fc46581966c425d551708529d5d30f673
Hao-Wei Pang <hwpang@mit.edu>
2022-11-03 16:58:19 -0400

    process FF conf

--- main_process_FF_conf_result_parallel.py
+++ main_process_FF_conf_result_parallel.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from argparse import ArgumentParser
-
 import os
 import re
 import sys
@@ -12,29 +10,40 @@ import numpy as np
 import pandas as pd
 import pickle as pkl
 
-from lib.file_parser import load_sdf
-
 from joblib import Parallel, delayed
+from rdkit import Chem
 
 def parser(mol_confs_sdf):
 
     failed_jobs = dict()
     valid_mol = dict()
-
+    
     mol_id = os.path.basename(mol_confs_sdf).split("_confs.sdf")[0]
     mol_smi = df.loc[df['id'] == mol_id]['smiles'].tolist()[0]
     pre_adj = Chem.GetAdjacencyMatrix(Chem.MolFromSmiles(mol_smi))
-
-    opt_mol = load_sdf(mol_confs_sdf)
-    conf_ids = list(range(opt_mol.GetNumConformers()))
-    for conf_id in conf_ids:
-        xyz = Chem.MolToXYZBlock(opt_mol, conf_id)
-        en = opt_mol.ConfEnergies
+    
+    failed_jobs[mol_id] = dict()
+    valid_mol[mol_id] = dict()
+    
+    mols = Chem.SDMolSupplier(mol_confs_sdf, removeHs=False, sanitize=True)
+    for conf_id, mol in enumerate(mols):
+        try:
+            xyz = Chem.MolToXYZBlock(mol)
+            en = mol.GetProp("ConfEnergies")
+            valid_mol[mol_id][conf_id]["ff_xyz"] = xyz
+            valid_mol[mol_id][conf_id]["ff_en"] = en
+        except:
+            failed_jobs[mol_id][conf_id] = "failed"
+            
+        
     return failed_jobs, valid_mol
 
 input_smiles_path = sys.argv[1]
 output_file_name = sys.argv[2]
 n_jobs = int(sys.argv[3])
+
+# input_smiles_path = "reactants_products_wb97xd_and_xtb_opted_ts_combo_results_hashed_chart_aug11b.csv"
+# n_jobs = 8
 
 df = pd.read_csv(input_smiles_path)
 mol_confs_sdf_paths = []
