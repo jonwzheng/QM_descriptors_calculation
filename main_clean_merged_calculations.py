d5dda77bfc24e89c18f538570f1f5c3bad14ab32
Hao-Wei Pang <hwpang@mit.edu>
2022-11-03 16:10:42 -0400

    add clean

--- main_clean_merged_calculations.py
+++ main_clean_merged_calculations.py
@@ -3,6 +3,19 @@ import shutil
 
 submit_dir = os.getcwd()
 
+# for suboutput_folder in os.listdir(os.path.join(submit_dir, "output", "FF_conf", "outputs")):
+#     for job_folder in os.listdir(os.path.join(submit_dir, "output", "FF_conf", "outputs", suboutput_folder)):
+#         mol_id = job_folder
+#         if os.path.exists(os.path.join(submit_dir, "output", "FF_conf", "outputs", suboutput_folder, job_folder, mol_id+"_confs.sdf")):
+#             shutil.move(os.path.join(submit_dir, "output", "FF_conf", "outputs", suboutput_folder, job_folder, mol_id+"_confs.sdf"), os.path.join(submit_dir, "output", "FF_conf", "outputs", suboutput_folder, mol_id+"_confs.sdf"))
+#             print(os.path.join(submit_dir, "output", "FF_conf", "outputs", suboutput_folder, mol_id+"_confs.sdf"))
+#             shutil.rmtree(os.path.join(submit_dir, "output", "FF_conf", "outputs", suboutput_folder, job_folder))
+
+# for suboutput_folder in os.listdir(os.path.join(submit_dir, "output", "FF_conf", "outputs")):
+#     if not any("_confs.sdf" in file for file in os.listdir(os.path.join(submit_dir, "output", "FF_conf", "outputs", suboutput_folder))):
+#         print(os.path.join(submit_dir, "output", "FF_conf", "outputs", suboutput_folder))
+        # shutil.rmtree(os.path.join(submit_dir, "output", "FF_conf", "outputs", suboutput_folder))
+
 # for suboutput_folder in os.listdir(os.path.join(submit_dir, "output", "DFT_opt_freq", "outputs")):
 #     for job_folder in os.listdir(os.path.join(submit_dir, "output", "DFT_opt_freq", "outputs", suboutput_folder)):
 #         mol_id = job_folder
@@ -10,11 +23,3 @@ submit_dir = os.getcwd()
 #             shutil.move(os.path.join(submit_dir, "output", "DFT_opt_freq", "outputs", suboutput_folder, job_folder, mol_id+".log"), os.path.join(submit_dir, "output", "DFT_opt_freq", "outputs", suboutput_folder, mol_id+".log"))
 #             print(os.path.join(submit_dir, "output", "DFT_opt_freq", "outputs", suboutput_folder, mol_id+".log"))
 #             shutil.rmtree(os.path.join(submit_dir, "output", "DFT_opt_freq", "outputs", suboutput_folder, job_folder))
-
-for suboutput_folder in os.listdir(os.path.join(submit_dir, "output", "FF_conf", "outputs")):
-    for job_folder in os.listdir(os.path.join(submit_dir, "output", "FF_conf", "outputs", suboutput_folder)):
-        mol_id = job_folder
-        if os.path.exists(os.path.join(submit_dir, "output", "FF_conf", "outputs", suboutput_folder, job_folder, mol_id+"_confs.sdf")):
-            shutil.move(os.path.join(submit_dir, "output", "FF_conf", "outputs", suboutput_folder, job_folder, mol_id+"_confs.sdf"), os.path.join(submit_dir, "output", "FF_conf", "outputs", suboutput_folder, mol_id+"_confs.sdf"))
-            print(os.path.join(submit_dir, "output", "FF_conf", "outputs", suboutput_folder, mol_id+"_confs.sdf"))
-            shutil.rmtree(os.path.join(submit_dir, "output", "FF_conf", "outputs", suboutput_folder, job_folder))
