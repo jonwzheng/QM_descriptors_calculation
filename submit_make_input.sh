0930a6b5e41bcc74e57c6eb9bbaf30835f6c99d0
Hao-Wei Pang <hwpang@mit.edu>
2022-11-08 17:43:46 -0500

    update make input script

--- submit_make_input.sh
+++ submit_make_input.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -l
+#SBATCH --array=0-49
 
 echo "============================================================"
 echo "Job ID : $SLURM_JOB_ID"
@@ -8,7 +9,7 @@ echo "Running on node : $SLURMD_NODENAME"
 echo "Current directory : $(pwd)"
 echo "============================================================"
 
-conda activate rdmc_env
+#conda activate rdmc_env
 
 #xtb
 source /home/gridsan/groups/RMG/Software/xtb-6.4.1/share/xtb/config_env.bash
@@ -27,10 +28,11 @@ export GAUSS_SCRDIR=""
 
 #QMD
 QMD_PATH=/home/gridsan/groups/RMG/Software/QM_descriptors_calculation-radical_workflow
-#input_smiles=/home/gridsan/groups/RMG/Projects/Hao-Wei-Oscar-Yunsie/production_run/HAbs/inputs/TS_cosmo_dlpno/wb97xd_and_xtb_opted_ts_combo_results_hashed_sep1a_ts_input.csv
+input_smiles=reactants_products_wb97xd_and_xtb_opted_ts_combo_results_hashed_chart_aug11b.csv
+xyz_FF_dict=reactants_products_aug11b_ff_opted_results_xyz.pkl
 
 # #ff conf
 # python $QMD_PATH/main_make_FF_conf_input.py --input_smiles $input_smiles --XTB_path $XTB_PATH 
 
 #semi opt
-python $QMD_PATH/main_make_semiempirical_opt_input.py --input_smiles $input_smiles --XTB_path $XTB_PATH --RDMC_path $RDMC_PATH --G16_path $g16root
+python $QMD_PATH/main_make_semiempirical_opt_input.py --input_smiles $input_smiles --XTB_path $XTB_PATH --RDMC_path $RDMC_PATH --G16_path $g16root --xyz_FF_dict $xyz_FF_dict
