875f55ee4b15524919536a66cc5b61836d16bafc
Hao-Wei Pang <hwpang@mit.edu>
2022-11-08 18:01:50 -0500

    new semi opt submit script

---
+++ submit_semiempirical_opt_array.sh
@@ -0,0 +1,37 @@
+#!/bin/bash -l
+
+echo "============================================================"
+echo "Job ID : $SLURM_JOB_ID"
+echo "Job Name : $SLURM_JOB_NAME"
+echo "Starting on : $(date)"
+echo "Running on node : $SLURMD_NODENAME"
+echo "Current directory : $(pwd)"
+echo "============================================================"
+
+# conda activate rdmc_env
+#xtb
+source /home/gridsan/groups/RMG/Software/xtb-6.4.1/share/xtb/config_env.bash
+XTB_PATH=/home/gridsan/groups/RMG/Software/xtb-6.4.1/bin
+export PATH=$XTB_PATH:$PATH
+
+#RDMC for gaussian-xtb
+RDMC_PATH=/home/gridsan/groups/RMG/Software/RDMC-main
+
+#gaussian
+export PATH=$PATH:/home/gridsan/groups/RMG/Software/gaussian/g16
+export PATH=$PATH:/home/gridsan/groups/RMG/Software/gaussian/gv
+export g16root=/home/gridsan/groups/RMG/Software/gaussian
+source /home/gridsan/groups/RMG/Software/gaussian/g16/bsd/g16.profile
+export GAUSS_SCRDIR=""
+
+#QMD
+QMD_PATH=/home/gridsan/groups/RMG/Software/QM_descriptors_calculation-radical_workflow
+input_smiles=reactants_products_wb97xd_and_xtb_opted_ts_combo_results_hashed_chart_aug11b.csv
+xyz_FF_dict=reactants_products_aug11b_ff_opted_results_xyz.pkl
+
+scratch_dir=$TMPDIR/$USER/$SLURM_JOB_ID-$SLURM_ARRAY_TASK_ID
+mkdir -p $scratch_dir
+
+python $QMD_PATH/main_make_semiempirical_opt_input.py --input_smiles $input_smiles --XTB_path $XTB_PATH --RDMC_path $RDMC_PATH --G16_path $g16root --xyz_FF_dict $xyz_FF_dict --scratch_dir $scratch_dir
+
+rm -rf $scratch_dir
