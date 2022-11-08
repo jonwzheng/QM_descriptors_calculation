d25f2681107eed55976968e93bf6d6a42b74a448
Hao-Wei Pang <hwpang@mit.edu>
2022-11-08 18:56:54 -0500

    add title card

--- lib/semiempirical_calculation.py
+++ lib/semiempirical_calculation.py
@@ -20,6 +20,8 @@ def semiempirical_opt(mol_id, base_charge, mult, xyz_FF_dict, xtb_path, rdmc_pat
         logfile = f"{mol_id}_{conf_ind}.log"
         outfile = f"{mol_id}_{conf_ind}.out"
 
+        xyz = "Title\n"+xyz
+
         if os.path.exists(os.path.join(tmp_mol_dir, logfile)):
             continue
 
@@ -60,9 +62,9 @@ def semiempirical_opt(mol_id, base_charge, mult, xyz_FF_dict, xtb_path, rdmc_pat
         tar.add(os.path.join(tmp_mol_dir, logfile))
     tar.close()
 
-    shutil.copy(tar_file, os.path.join(suboutputs_dir, tar_file))
-    os.remove(os.path.join(subinputs_dir, f"{mol_id}.tmp"))
-    shutil.rmtree(tmp_mol_dir)
+    # shutil.copy(tar_file, os.path.join(suboutputs_dir, tar_file))
+    # os.remove(os.path.join(subinputs_dir, f"{mol_id}.tmp"))
+    # shutil.rmtree(tmp_mol_dir)
     os.chdir(current_dir)
 
 def xtb_status(folder, molid):
