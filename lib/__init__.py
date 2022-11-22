00c432fd9a77a5048da7c71bb9bfea321860e3a0
Hao-Wei Pang <hwpang@mit.edu>
2022-11-22 14:50:40 -0500

    remove old functions

--- lib/__init__.py
+++ lib/__init__.py
@@ -2,7 +2,7 @@ from .dft_calculation import dft_scf_qm_descriptor, dft_scf_opt, dft_scf_sp, sav
 from .file_parser import mol2xyz, xyz2com
 from .log_parser import G16Log, XtbLog
 from .grab_QM_descriptors import read_log
-from .utils import create_logger, DoneJobsRecord, REPLACE_LETTER
+from .utils import REPLACE_LETTER
 from .semiempirical_calculation import semiempirical_opt, xtb_status
 from .cosmo_calculation import cosmo_calc
 from .wft_calculation import dlpno_sp_calc
