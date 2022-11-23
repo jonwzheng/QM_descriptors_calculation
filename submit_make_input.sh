db4d12de59b3cdda8dd290b61fb3e2343dd758bc
Hao-Wei Pang <hwpang@mit.edu>
2022-11-23 14:08:42 -0500

    xyz dft opt dict

--- submit_make_input.sh
+++ submit_make_input.sh
@@ -26,20 +26,27 @@ export g16root=/home/gridsan/groups/RMG/Software/gaussian
 source /home/gridsan/groups/RMG/Software/gaussian/g16/bsd/g16.profile
 export GAUSS_SCRDIR=""
 
+#COSMO
+TURBODIR=/home/gridsan/groups/RMG/Software/TmoleX19/TURBOMOLE
+source $TURBODIR/Config_turbo_env
+COSMOTHERM_PATH=/home/gridsan/groups/RMG/Software/COSMOtherm2021
+COSMO_DATABASE_PATH=/home/gridsan/groups/RMG/COSMO_database/COSMObase2021
+
 #QMD
 QMD_PATH=/home/gridsan/groups/RMG/Software/QM_descriptors_calculation-radical_workflow
+input_smiles=reactants_products_wb97xd_and_xtb_opted_ts_combo_results_hashed_chart_aug11b.csv
+xyz_FF_dict=reactants_products_aug11b_ff_opted_results_xyz.pkl
+xyz_semiempirical_opt_dict=reactants_products_aug11b_semiempirical_opted_results_xyz.pkl
+xyz_DFT_opt_dict=reactants_products_aug11b_dft_opted_results_xyz.pkl
 
-#inputs
-input_smiles=reactants_products_wb97xd_and_xtb_opted_ts_combo_results_hashed_lookup_table_sep1a_filtered.csv
-output_name=reactants_products_sep1a_filtered
-xyz_FF_dict=${output_name}_ff_opted_results_xyz.pkl
-xyz_semiempirical_opt_dict=${output_name}_semiempirical_opted_results_xyz.pkl
-
-#ff conf
-python $QMD_PATH/main_make_FF_conf_input.py --input_smiles $input_smiles --XTB_path $XTB_PATH 
+# #ff conf
+# python $QMD_PATH/main_make_FF_conf_input.py --input_smiles $input_smiles --XTB_path $XTB_PATH 
 
 # #semi opt
 # python $QMD_PATH/main_make_semiempirical_opt_input.py --input_smiles $input_smiles --XTB_path $XTB_PATH --RDMC_path $RDMC_PATH --G16_path $g16root --xyz_FF_dict $xyz_FF_dict
 
 # #dft opt freq
 # python $QMD_PATH/main_make_DFT_opt_freq_input.py --input_smiles $input_smiles --G16_path $g16root --xyz_semiempirical_opt_dict $xyz_semiempirical_opt_dict
+
+#cosmo
+python $QMD_PATH/main_make_cosmo_input.py --input_smiles $input_smiles --xyz_DFT_opt_dict $xyz_DFT_opt_dict --COSMOtherm_path $COSMOTHERM_PATH --COSMO_database_path $COSMO_DATABASE_PATH
