b0f7afb44bc2866477eea528c1641f392719be01
Hao-Wei Pang <hwpang@mit.edu>
2022-11-28 15:41:14 -0500

    make input for all

--- submit_make_input.sh
+++ submit_make_input.sh
@@ -34,12 +34,28 @@ source $TURBODIR/Config_turbo_env
 COSMOTHERM_PATH=/home/gridsan/groups/RMG/Software/COSMOtherm2021
 COSMO_DATABASE_PATH=/home/gridsan/groups/RMG/COSMO_database/COSMObase2021
 
+#openmpi
+ompi=/home/gridsan/groups/RMG/Software/ompi-3.1.4
+PATH=$ompi/bin:$PATH
+LD_LIBRARY_PATH=$ompi/lib:$ompi/etc:$LD_LIBRARY_PATH
+
+#Orca
+orcadir=/home/gridsan/groups/RMG/Software/orca_4_2_1_linux_x86-64_openmpi314
+export PATH=$PATH:$orcadir
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$orcadir
+echo "orcaversion"
+which orca
+echo $PATH
+
 #QMD
 QMD_PATH=/home/gridsan/groups/RMG/Software/QM_descriptors_calculation-radical_workflow
-input_smiles=reactants_products_wb97xd_and_xtb_opted_ts_combo_results_hashed_chart_aug11b.csv
-xyz_FF_dict=reactants_products_aug11b_ff_opted_results_xyz.pkl
-xyz_semiempirical_opt_dict=reactants_products_aug11b_semiempirical_opted_results_xyz.pkl
-xyz_DFT_opt_dict=reactants_products_aug11b_dft_opted_results_xyz.pkl
+
+#inputs
+input_smiles=reactants_products_wb97xd_and_xtb_opted_ts_combo_results_hashed_lookup_table_sep1a_filtered.csv
+output_name=reactants_products_sep1a_filtered
+xyz_FF_dict=${output_name}_ff_opted_results_xyz.pkl
+xyz_semiempirical_opt_dict=${output_name}_semiempirical_opted_results_xyz.pkl
+xyz_DFT_opt_dict=${output_name}_dft_opted_results_xyz.pkl
 
 # #ff conf
 # python $QMD_PATH/main_make_FF_conf_input.py --input_smiles $input_smiles --XTB_path $XTB_PATH 
@@ -50,5 +66,8 @@ xyz_DFT_opt_dict=reactants_products_aug11b_dft_opted_results_xyz.pkl
 # #dft opt freq
 # python $QMD_PATH/main_make_DFT_opt_freq_input.py --input_smiles $input_smiles --G16_path $g16root/g16 --xyz_semiempirical_opt_dict $xyz_semiempirical_opt_dict
 
-#cosmo
-python $QMD_PATH/main_make_cosmo_input.py --input_smiles $input_smiles --xyz_DFT_opt_dict $xyz_DFT_opt_dict --COSMOtherm_path $COSMOTHERM_PATH --COSMO_database_path $COSMO_DATABASE_PATH
+# #cosmo
+# python $QMD_PATH/main_make_cosmo_input.py --input_smiles $input_smiles --xyz_DFT_opt_dict $xyz_DFT_opt_dict --COSMOtherm_path $COSMOTHERM_PATH --COSMO_database_path $COSMO_DATABASE_PATH
+
+#dlpno
+python $QMD_PATH/main_make_dlpno_sp_input.py --input_smiles $input_smiles --xyz_DFT_opt_dict $xyz_DFT_opt_dict --DLPNO_sp_n_procs 22 --DLPNO_sp_job_ram 3900 --ORCA_path $orcadir
