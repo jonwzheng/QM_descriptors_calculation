83129b8313ae6db866cc77ba257a84b91561728c
Hao-Wei Pang <hwpang@mit.edu>
2022-11-28 15:16:31 -0500

    process r p complex FF opt

--- main_process_r_p_complex_FF_opt_result_parallel.py
+++ main_process_r_p_complex_FF_opt_result_parallel.py
@@ -9,31 +9,38 @@ from joblib import Parallel, delayed
 from rdkit import Chem
 from rdmc.mol import RDKitMol
 
-def parser(ts_id, tar_file_path):
-    tar = tarfile.open(tar_file_path)
-    ts_smi = ts_id_to_smi[ts_id]
-    r_smi, p_smi = ts_smi.split(">>")
-    ts_xyz = ts_id_to_xyz[ts_id]
-    ts_mol = RDKitMol.FromXYZ(ts_xyz, sanitize=False)
-    ts_mol.SetProp("_Name", ts_id)
-    for member in tar:
-        if "_r.log" in member.name:
-            f = tar.extractfile(member)
-            suppl = Chem.ForwardSDMolSupplier(f)
-            r_mol = [mol for mol in suppl][0]
-            r_mol.SetProp("_Name", r_smi)
-        elif "_p.log" in member.name:
-            f = tar.extractfile(member)
-            suppl = Chem.ForwardSDMolSupplier(f)
-            p_mol = [mol for mol in suppl][0]
-            p_mol.SetProp("_Name", p_smi)
-    tar.close()
-    return ts_id, r_smi, p_smi, ts_mol, r_mol, p_mol
+def parser(ts_id, submit_dir):
+    ids = str(int(int(ts_id.split("id")[1])/1000))
+    tar_file_path = os.path.join(submit_dir, "output", "r_p_complex_FF_opt", "outputs", f"outputs_{ids}", f"{ts_id}.tar")
+    if os.path.exists(tar_file_path):
+        tar = tarfile.open(tar_file_path)
+        ts_smi = ts_id_to_smi[ts_id]
+        r_smi, p_smi = ts_smi.split(">>")
+        ts_xyz = ts_id_to_xyz[ts_id]
+        ts_mol = RDKitMol.FromXYZ(ts_xyz, sanitize=False)
+        ts_mol.SetProp("_Name", ts_id)
+        for member in tar:
+            if "_r.log" in member.name:
+                f = tar.extractfile(member)
+                suppl = Chem.ForwardSDMolSupplier(f)
+                r_mol = [mol for mol in suppl][0]
+                r_mol.SetProp("_Name", r_smi)
+            elif "_p.log" in member.name:
+                f = tar.extractfile(member)
+                suppl = Chem.ForwardSDMolSupplier(f)
+                p_mol = [mol for mol in suppl][0]
+                p_mol.SetProp("_Name", p_smi)
+        tar.close()
+        return ts_id, r_smi, p_smi, ts_mol, r_mol, p_mol
+    else:
+        return None
 
 input_smiles_path = sys.argv[1]
 output_file_name = sys.argv[2]
 n_jobs = int(sys.argv[3])
 ts_id_to_xyz_path = sys.argv[4]
+
+submit_dir = os.getcwd()
 
 # input_smiles_path = "reactants_products_wb97xd_and_xtb_opted_ts_combo_results_hashed_chart_aug11b.csv"
 # n_jobs = 8
@@ -45,19 +52,8 @@ ts_ids = list(df.id)
 with open(ts_id_to_xyz_path, "rb") as f:
     ts_id_to_xyz = pkl.load(f)
 
-tar_file_paths = []
-submit_dir = os.getcwd()
-ts_id_save = []
-
-for ts_id in ts_ids:
-    ids = str(int(int(ts_id.split("id")[1])/1000))
-    tar_file_path = os.path.join(submit_dir, "output", "r_p_complex_FF_opt", "outputs", f"outputs_{ids}", f"{ts_id}.tar")
-    if os.path.exists(tar_file_path):
-        tar_file_paths.append(tar_file_path)
-        ts_id_save.append(ts_id)
-
-out = Parallel(n_jobs=n_jobs, backend="multiprocessing", verbose=5)(delayed(parser)(ts_id, tar_file) for ts_id, tar_file in zip(ts_id_save, tar_file_paths))
-
+out = Parallel(n_jobs=n_jobs, backend="multiprocessing", verbose=5)(delayed(parser)(ts_id, submit_dir) for ts_id in ts_ids)
+out = [x for x in out if x is not None]
 csv_file = os.path.join(submit_dir, f'{output_file_name}.csv')
 ts_writer = Chem.rdmolfiles.SDWriter(os.path.join(submit_dir, f'{output_file_name}_ts.sdf'))
 r_writer = Chem.rdmolfiles.SDWriter(os.path.join(submit_dir, f'{output_file_name}_reactants.sdf'))
