dee17ec568026d552cf1665223c9bd2b46c460e3
Hao-Wei Pang <hwpang@mit.edu>
2023-01-30 20:25:12 -0500

    copyfile

--- lib/FF_conf_generation.py
+++ lib/FF_conf_generation.py
@@ -59,7 +59,7 @@ def _genConf(smi, mol_id, XTB_path, conf_search_FF, max_n_conf, max_try, rms, E_
                     try:
                         en = float(log.E)
                     except:
-                        os.copyfile(output_file_mol_id, os.path.join(current_dir, output_file_mol_id))
+                        shutil.copyfile(output_file_mol_id, os.path.join(current_dir, output_file_mol_id))
                         raise
                     opt_mol = load_sdf("xtbopt.sdf")[0]
                     post_adj = Chem.GetAdjacencyMatrix(opt_mol)
