712031e47c3583ccbe9ca6acc53235c953f01503
Hao-Wei Pang <hwpang@mit.edu>
2023-02-02 09:13:58 -0500

    don't remove tmp when failed

--- lib/dft_calculation.py
+++ lib/dft_calculation.py
@@ -133,10 +133,6 @@ def dft_scf_opt(mol_id, mol_smi, xyz_semiempirical_opt_dict, g16_path, DFT_opt_f
                 lines = f.readlines()
             print("\n".join(lines[-10:]))
             shutil.copyfile(logfile, os.path.join(suboutputs_dir, logfile))
-            try:
-                os.remove(os.path.join(subinputs_dir, f"{mol_id}.tmp"))
-            except FileNotFoundError:
-                pass
             os.chdir(current_dir)
             shutil.rmtree(mol_scratch_dir)
             print(f"Optimization of {mol_id} with {level_of_theory} didn't converge.")
