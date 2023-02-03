202ca15ae438d7070685208c88bc4c8c81064d13
Hao-Wei Pang <hwpang@mit.edu>
2023-02-03 10:22:44 -0500

    move ff parser

---
+++ lib/parser/ff_conf_parser.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+from rdmc.mol import RDKitMol
+
+def ff_conf_parser(mol_id, mol_smi):
+
+    failed_job = dict()
+    valid_job = dict()
+
+    ids = str(int(int(mol_id.split("id")[1])/1000)) 
+    mol_confs_sdf = os.path.join("output", "FF_conf", "outputs", f"outputs_{ids}", f"{mol_id}_confs.sdf")
+
+    if os.path.isfile(mol_confs_sdf):
+
+        failed_job[mol_id] = dict()
+        valid_job[mol_id] = dict()
+
+        pre_adj = RDKitMol.FromSmiles(mol_smi).GetAdjacencyMatrix()
+        
+        mols = RDKitMol.FromFile(mol_confs_sdf)
+        for conf_id, mol in enumerate(mols):
+            post_adj = mol.GetAdjacencyMatrix()
+            try:
+                (pre_adj == post_adj).all()
+            except:
+                print(mol_confs_sdf)
+                break
+            
+            if (pre_adj == post_adj).all():
+                valid_job[mol_id][conf_id] = {}
+                xyz = mol.ToXYZ()
+                en = mol.GetProp("ConfEnergies")
+                valid_job[mol_id][conf_id]["ff_xyz"] = xyz
+                valid_job[mol_id][conf_id]["ff_energy"] = en
+            else:
+                failed_job[mol_id][conf_id] = 'adjacency matrix'
+        
+        if not valid_job[mol_id]:
+            del valid_job[mol_id]
+            failed_job[mol_id]['reason'] = 'all confs failed'
+        else:
+            if not failed_job[mol_id]:
+                del failed_job[mol_id]
+    else:
+        failed_job[mol_id] = dict()
+        failed_job[mol_id]['reason'] = 'file not found'
+
+    return failed_job, valid_job
--- main_process_FF_conf_result_parallel.py
+++ main_process_FF_conf_result_parallel.py
@@ -2,81 +2,22 @@
 # coding: utf-8
 
 import os
-import re
 import sys
-import shutil
-
-import numpy as np
 import pandas as pd
 import pickle as pkl
-
 from joblib import Parallel, delayed
-from rdmc.mol import RDKitMol
 
-def parser(mol_id):
-
-    ids = str(int(int(mol_id.split("id")[1])/1000)) 
-    mol_confs_sdf = os.path.join(submit_dir, "output", "FF_conf", "outputs", f"outputs_{ids}", f"{mol_id}_confs.sdf")
-    failed_job = dict()
-    valid_job = dict()
-
-    if os.path.isfile(mol_confs_sdf):
-
-        failed_job[mol_id] = dict()
-        valid_job[mol_id] = dict()
-
-        mol_smi = mol_id_to_smi[mol_id]
-        pre_adj = RDKitMol.FromSmiles(mol_smi).GetAdjacencyMatrix()
-        
-        mols = RDKitMol.FromFile(mol_confs_sdf)
-        for conf_id, mol in enumerate(mols):
-            post_adj = mol.GetAdjacencyMatrix()
-            try:
-                (pre_adj == post_adj).all()
-            except:
-                print(mol_confs_sdf)
-                break
-            
-            if (pre_adj == post_adj).all():
-                valid_job[mol_id][conf_id] = {}
-                xyz = mol.ToXYZ()
-                en = mol.GetProp("ConfEnergies")
-                valid_job[mol_id][conf_id]["ff_xyz"] = xyz
-                valid_job[mol_id][conf_id]["ff_energy"] = en
-            else:
-                failed_job[mol_id][conf_id] = 'adjacency matrix'
-        
-        if not valid_job[mol_id]:
-            del valid_job[mol_id]
-            failed_job[mol_id]['reason'] = 'all confs failed'
-        else:
-            if not failed_job[mol_id]:
-                del failed_job[mol_id]
-    else:
-        failed_job[mol_id] = dict()
-        failed_job[mol_id]['reason'] = 'file not found'
-
-    return failed_job, valid_job
+from lib.parser.ff_conf_parser import ff_conf_parser as parser
 
 input_smiles_path = sys.argv[1]
 output_file_name = sys.argv[2]
 n_jobs = int(sys.argv[3])
 
-submit_dir = os.getcwd()
-
-##
-# input_smiles_path = "inputs/reactants_products_aug11b_inputs.csv"
-# output_file_name = "reactants_products_aug11b"
-# n_jobs = 1
-
 df = pd.read_csv(input_smiles_path)
 mol_ids = list(df.id)
 mol_id_to_smi = dict(zip(df.id, df.smiles))
 
-##
-# mol_ids = mol_ids[:1000]
-
-out = Parallel(n_jobs=n_jobs, backend="multiprocessing", verbose=5)(delayed(parser)(mol_id) for mol_id in mol_ids)
+out = Parallel(n_jobs=n_jobs, backend="multiprocessing", verbose=5)(delayed(parser)(mol_id, mol_id_to_smi[mol_id]) for mol_id in mol_ids)
 
 failed_jobs = dict()
 valid_jobs = dict()
@@ -84,17 +25,14 @@ for failed_job, valid_job in out:
     failed_jobs.update(failed_job)
     valid_jobs.update(valid_job)
 
-with open(os.path.join(submit_dir, f'{output_file_name}.pkl'), 'wb') as outfile:
+with open(os.path.join(f'{output_file_name}.pkl'), 'wb') as outfile:
     pkl.dump(valid_jobs, outfile, protocol=pkl.HIGHEST_PROTOCOL)
 
-with open(os.path.join(submit_dir, f'{output_file_name}_failed.pkl'), 'wb') as outfile:
+with open(os.path.join(f'{output_file_name}_failed.pkl'), 'wb') as outfile:
     pkl.dump(failed_jobs, outfile, protocol=pkl.HIGHEST_PROTOCOL)
 
+print(f"Total number of molecules: {len(mol_ids)}")
+print(f"Total number of failed molecules: {len(failed_jobs)}")
 print(failed_jobs)
 
-xyz_FF_opt = {}
-for mol_id in valid_jobs:
-    xyz_FF_opt[mol_id] = {conf_id: valid_jobs[mol_id][conf_id]["ff_xyz"] for conf_id in valid_jobs[mol_id]}
-
-with open(f"{output_file_name}_xyz.pkl", "wb") as f:
-    pkl.dump(xyz_FF_opt, f, protocol=pkl.HIGHEST_PROTOCOL)
+print("Done!")
