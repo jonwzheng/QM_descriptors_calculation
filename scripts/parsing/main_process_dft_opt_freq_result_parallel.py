4bc37857f5d42e3c8fcea7ccce341346f9882d34
Hao-Wei Pang <hwpang@mit.edu>
2023-02-06 13:28:26 -0500

    save dft xyz

--- scripts/parsing/main_process_dft_opt_freq_result_parallel.py
+++ scripts/parsing/main_process_dft_opt_freq_result_parallel.py
@@ -34,6 +34,13 @@ with open(os.path.join(f'{output_file_name}.pkl'), 'wb') as outfile:
 with open(os.path.join(f'{output_file_name}_failed.pkl'), 'wb') as outfile:
     pkl.dump(failed_jobs, outfile, protocol=pkl.HIGHEST_PROTOCOL)
 
+mol_id_to_DFT_opted_xyz = {}
+for mol_id, valid_job in valid_jobs.items():
+    mol_id_to_DFT_opted_xyz[mol_id] = valid_job['dft_xyz']
+
+with open(os.path.join(f'{output_file_name}_xyz.pkl'), 'wb') as outfile:
+    pkl.dump(mol_id_to_DFT_opted_xyz, outfile, protocol=pkl.HIGHEST_PROTOCOL)
+
 print(f"Total number of molecules: {len(mol_ids)}")
 print(f"Total number of failed molecules: {len(failed_jobs)}")
 print(failed_jobs)
