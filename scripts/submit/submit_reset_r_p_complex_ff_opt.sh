9ac867a009625dd01bc2554371d82e67f429e81b
Hao-Wei Pang <hwpang@mit.edu>
2023-02-09 17:59:45 -0500

    update

--- scripts/submit/submit_reset_r_p_complex_ff_opt.sh
+++ scripts/submit/submit_reset_r_p_complex_ff_opt.sh
@@ -8,19 +8,20 @@ echo "Running on node : $SLURMD_NODENAME"
 echo "Current directory : $(pwd)"
 echo "============================================================"
 
-source /home/gridsan/groups/RMG/Software/xtb-6.4.1/share/xtb/config_env.bas
+source /state/partition1/llgrid/pkg/anaconda/anaconda3-2022b/etc/profile.d/conda.sh
 conda activate rdmc_env
+which python
 
 #QMD
 QMD_PATH=/home/gridsan/groups/RMG/Software/QM_descriptors_calculation-radical_workflow
 export PYTHONPATH=$QMD_PATH:$PYTHONPATH
-input_smiles=/home/gridsan/hwpang/RMG_shared/Projects/Hao-Wei-Oscar-Yunsie/HAbs_calculations/ts_results/roo_ts_checked_feb9a.pkl
+input_smiles=../../../ts_calculations/calculations/feb9a/inputs/ts_feb9a_inputs.csv
 
 scratch_dir=$TMPDIR/$USER/$SLURM_JOB_ID-$SLURM_ARRAY_TASK_ID-$LLSUB_RANK-$LLSUB_SIZE
 mkdir -p $scratch_dir
 echo $scratch_dir
 
 #r p complex semi opt
-python $QMD_PATH/scripts/calculation/ff_opt_r_p_complex.py.py --input_smiles $input_smiles --RDMC_path $RDMC_PATH --xyz_DFT_opt_dict $xyz_DFT_opt_dict --scratch_dir $scratch_dir
+python $QMD_PATH/scripts/calculation/ff_opt_r_p_complex.py.py --input_smiles $input_smiles --RDMC_path $RDMC_PATH --scratch_dir $scratch_dir
 
 rm -rf $scratch_dir
