3a00ea039ab8692ee21e3afd3c7aa4909c16f851
Hao-Wei Pang <hwpang@mit.edu>
2023-02-14 15:11:23 -0500

    update

--- scripts/submit/submit_dlpno_array.sh
+++ scripts/submit/submit_dlpno_array.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -l
 #SBATCH -J orca
+#SBATCH -o slurm-orca_%A-%a.out
 #SBATCH --ntasks=24
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=4000
 #SBATCH --array=0-31
-#SBATCH -o slurm-orca_%a.out
 
 echo "============================================================"
 echo "Job ID : $SLURM_JOB_ID"
@@ -17,6 +17,15 @@ echo "============================================================"
 source /state/partition1/llgrid/pkg/anaconda/anaconda3-2022b/etc/profile.d/conda.sh
 conda activate rdmc_env
 which python
+
+#RDMC
+RDMC_PATH=/home/gridsan/groups/RMG/Software/RDMC-main
+export PATH=$RDMC_PATH:$PATH
+export PYTHONPATH=$PYTHONPATH:$RDMC_PATH
+
+#QMD
+QMD_PATH=/home/gridsan/groups/RMG/Software/radical_workflow
+export PYTHONPATH=$PYTHONPATH:$QMD_PATH
 
 #openmpi
 ompi=/home/gridsan/groups/RMG/Software/ompi-3.1.4
@@ -31,34 +40,29 @@ echo "orcaversion"
 which orca
 echo $PATH
 
-#QMD
-QMD_PATH=/home/gridsan/groups/RMG/Software/QM_descriptors_calculation-radical_workflow
-export PYTHONPATH=$QMD_PATH:$PYTHONPATH
-
 #inputs
-input_smiles=inputs/reactants_products_aug11b_inputs.csv
-output_name=reactants_products_aug11b
+input_smiles=$1
+output_name=$2
 xyz_DFT_opt_dict=${output_name}_dft_opted_results_xyz.pkl
 DLPNO_sp_n_procs=24
 DLPNO_sp_job_ram=4000
 
 #svp calculations
-
-DLPNO_folder='DLPNO_sp'
-DLPNO_level_of_theory='uHF dlpno-ccsd(t) def2-svp def2-svp/c TightSCF NormalPNO'
-echo $DLPNO_folder
+DLPNO_sp_folder="DLPNO_sp"
+DLPNO_level_of_theory="uHF dlpno-ccsd(t) def2-svp def2-svp/c TightSCF NormalPNO"
+echo $DLPNO_sp_folder
 echo $DLPNO_level_of_theory
 
 python -u $QMD_PATH/scripts/calculation/main_make_dlpno_sp_input.py \
 --input_smiles $input_smiles \
 --xyz_DFT_opt_dict $xyz_DFT_opt_dict \
---DLPNO_folder $DLPNO_folder \
---DLPNO_level_of_theory $DLPNO_level_of_theory \
+--DLPNO_sp_folder $DLPNO_sp_folder \
+--DLPNO_level_of_theory "$DLPNO_level_of_theory" \
 --DLPNO_sp_n_procs $DLPNO_sp_n_procs \
 --DLPNO_sp_job_ram $DLPNO_sp_job_ram \
---ORCA_path $orcadir \
 --task_id $SLURM_ARRAY_TASK_ID \
---num_tasks $SLURM_ARRAY_TASK_COUNT
+--num_tasks $SLURM_ARRAY_TASK_COUNT \
+--ORCA_path $orcadir
 
 SubmitDir=`pwd`
 
@@ -66,9 +70,9 @@ echo "Starting DLPNO calculations..."
 
 for i in {1..5}; do
     cd $SubmitDir
-    for folder in $SubmitDir/output/$DLPNO_folder/inputs/inputs_*; do
+    for folder in $SubmitDir/output/$DLPNO_sp_folder/inputs/inputs_*; do
 
-        folderind="${folder##$SubmitDir/output/$DLPNO_folder/inputs/inputs_}"
+        folderind="${folder##$SubmitDir/output/$DLPNO_sp_folder/inputs/inputs_}"
         echo "folderind $folderind"
         
         for filename in $folder/*.in; do
@@ -86,22 +90,23 @@ for i in {1..5}; do
                 cd $ScratchDir
                 cp $folder/$input.tmp $input.in
                 $orcadir/orca $input.in > $input.log
-                cp $input.log $SubmitDir/output/$DLPNO_folder/outputs/outputs_$folderind/
                 if [ -e $input.log ]
                 then
-                    if grep -Fq "aborting the run" $input.log
+                    if grep -Fq "ORCA TERMINATED NORMALLY" $input.log
                     then
-                        if grep -Fq "ORCA finished by" $input.log
+                        echo "done"
+                        cp $input.log $SubmitDir/output/$DLPNO_sp_folder/outputs/outputs_$folderind/
+                        rm $folder/$input.tmp
+                    else
+                        if grep -Fq "ORCA finished by error termination" $input.log
                         then
-                            echo "done but failed"
+                            echo "done with error termination"
+                            cp $input.log $SubmitDir/output/$DLPNO_sp_folder/outputs/outputs_$folderind/
                             rm $folder/$input.tmp
                         else
                             echo "failed"
                             mv $folder/$input.tmp $folder/$input.in
                         fi
-                    else
-                        echo "done"
-                        rm $folder/$input.tmp
                     fi
                 else
                     echo "failed"
@@ -115,16 +120,16 @@ for i in {1..5}; do
 done
 
 # f12 calculations
-DLPNO_folder='DLPNO_sp_f12'
-DLPNO_level_of_theory='uHF UNO DLPNO-CCSD(T)-F12D cc-pvtz-f12 def2/J cc-pvqz/c cc-pvqz-f12-cabs RIJCOSX VeryTightSCF NormalPNO'
-echo $DLPNO_folder
+DLPNO_sp_folder="DLPNO_sp_f12"
+DLPNO_level_of_theory="uHF UNO DLPNO-CCSD(T)-F12D cc-pvtz-f12 def2/J cc-pvqz/c cc-pvqz-f12-cabs RIJCOSX VeryTightSCF NormalPNO"
+echo $DLPNO_sp_folder
 echo $DLPNO_level_of_theory
 
 python -u $QMD_PATH/scripts/calculation/main_make_dlpno_sp_input.py \
 --input_smiles $input_smiles \
 --xyz_DFT_opt_dict $xyz_DFT_opt_dict \
---DLPNO_folder $DLPNO_folder \
---DLPNO_level_of_theory $DLPNO_level_of_theory \
+--DLPNO_sp_folder $DLPNO_sp_folder \
+--DLPNO_level_of_theory "$DLPNO_level_of_theory" \
 --DLPNO_sp_n_procs $DLPNO_sp_n_procs \
 --DLPNO_sp_job_ram $DLPNO_sp_job_ram \
 --ORCA_path $orcadir \
@@ -137,9 +142,9 @@ echo "Starting DLPNO calculations..."
 
 for i in {1..5}; do
     cd $SubmitDir
-    for folder in $SubmitDir/output/$DLPNO_folder/inputs/inputs_*; do
+    for folder in $SubmitDir/output/$DLPNO_sp_folder/inputs/inputs_*; do
 
-        folderind="${folder##$SubmitDir/output/$DLPNO_folder/inputs/inputs_}"
+        folderind="${folder##$SubmitDir/output/$DLPNO_sp_folder/inputs/inputs_}"
         echo "folderind $folderind"
         
         for filename in $folder/*.in; do
@@ -157,22 +162,24 @@ for i in {1..5}; do
                 cd $ScratchDir
                 cp $folder/$input.tmp $input.in
                 $orcadir/orca $input.in > $input.log
-                cp $input.log $SubmitDir/output/$DLPNO_folder/outputs/outputs_$folderind/
+                cp $input.log $SubmitDir/output/$DLPNO_sp_folder/outputs/outputs_$folderind/
                 if [ -e $input.log ]
                 then
-                    if grep -Fq "aborting the run" $input.log
+                    if grep -Fq "ORCA TERMINATED NORMALLY" $input.log
                     then
-                        if grep -Fq "ORCA finished by" $input.log
+                        echo "done"
+                        cp $input.log $SubmitDir/output/$DLPNO_sp_folder/outputs/outputs_$folderind/
+                        rm $folder/$input.tmp
+                    else
+                        if grep -Fq "ORCA finished by error termination" $input.log
                         then
-                            echo "done but failed"
+                            echo "done with error termination"
+                            cp $input.log $SubmitDir/output/$DLPNO_sp_folder/outputs/outputs_$folderind/
                             rm $folder/$input.tmp
                         else
                             echo "failed"
                             mv $folder/$input.tmp $folder/$input.in
                         fi
-                    else
-                        echo "done"
-                        rm $folder/$input.tmp
                     fi
                 else
                     echo "failed"
