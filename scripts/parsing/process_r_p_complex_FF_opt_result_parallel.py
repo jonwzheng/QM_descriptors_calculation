db955b1a85119485cbbc9baea188421c398198f7
Hao-Wei Pang <hwpang@mit.edu>
2023-02-15 14:32:43 -0500

    update r p complex

--- scripts/parsing/process_r_p_complex_FF_opt_result_parallel.py
+++ scripts/parsing/process_r_p_complex_FF_opt_result_parallel.py
@@ -9,45 +9,63 @@ from joblib import Parallel, delayed
 from rdkit import Chem
 from rdmc.mol import RDKitMol
 
-def parser(ts_id, submit_dir):
+def parser(ts_id):
     ids = str(int(int(ts_id.split("id")[1])/1000))
-    tar_file_path = os.path.join(submit_dir, "output", "r_p_complex_FF_opt", "outputs", f"outputs_{ids}", f"{ts_id}.tar")
+    ts_smi = ts_id_to_smi[ts_id]
+    r_smi, p_smi = ts_smi.split(">>")
+
+    pre_r_mol = Chem.MolFromSmiles(r_smi, removeHs=False, sanitize=False)
+    pre_p_mol = Chem.MolFromSmiles(p_smi, removeHs=False, sanitize=False)
+
+    pre_r_adj = pre_r_mol.GetAdjacencyMatrix()
+    pre_p_adj = pre_p_mol.GetAdjacencyMatrix()
+
+    ts_xyz = ts_id_to_xyz[ts_id]
+    ts_mol = RDKitMol.FromXYZ(ts_xyz, header=False, sanitize=False)
+    ts_mol._mol.SetProp("_Name", ts_id)
+    
+    tar_file_path = os.path.join("output", "r_p_complex_FF_opt", "outputs", f"outputs_{ids}", f"{ts_id}.tar")
+    sdf_file_path = os.path.join("output", "r_p_complex_FF_opt", "outputs", f"outputs_{ids}", f"{ts_id}_r_p.sdf")
     if os.path.exists(tar_file_path):
         tar = tarfile.open(tar_file_path)
-        ts_smi = ts_id_to_smi[ts_id]
-        r_smi, p_smi = ts_smi.split(">>")
-        ts_xyz = ts_id_to_xyz[ts_id]
-        ts_mol = RDKitMol.FromXYZ(ts_xyz, header=False, sanitize=False)
-        ts_mol._mol.SetProp("_Name", ts_id)
         for member in tar:
             if "_r.sdf" in member.name:
                 f = tar.extractfile(member)
                 mols = Chem.ForwardSDMolSupplier(f, removeHs=False)
                 r_mol = [mol for mol in mols][0]
                 r_mol = RDKitMol.FromMol(r_mol)
-                if any(atom.GetFormalCharge() != 0 for atom in r_mol._mol.GetAtoms()):
-                    return None
 
             elif "_p.sdf" in member.name:
                 f = tar.extractfile(member)
                 mols = Chem.ForwardSDMolSupplier(f, removeHs=False)
-                p_mol = [mol for mol in mols][0]    
+                p_mol = [mol for mol in mols][0]  
+                p_mol = RDKitMol.FromMol(p_mol)
         tar.close()
-        return ts_id, r_smi, p_smi, ts_mol, r_mol, p_mol
+    elif os.path.exists(sdf_file_path):
+        r_mol, p_mol = Chem.ForwardSDMolSupplier(sdf_file_path, removeHs=False, sanitize=False)
+        r_mol = RDKitMol.FromMol(r_mol)
+        p_mol = RDKitMol.FromMol(p_mol)
     else:
         return None
+
+    if any(atom.GetFormalCharge() != 0 for atom in r_mol.GetAtoms()):
+        return None
+    if any(atom.GetFormalCharge() != 0 for atom in p_mol.GetAtoms()):
+        return None
+
+    r_adj = r_mol.GetAdjacencyMatrix()
+    if not (r_adj == pre_r_adj).all():
+        return None
+    p_adj = p_mol.GetAdjacencyMatrix()
+    if not (p_adj == pre_p_adj).all():
+        return None
+    
+    return ts_id, r_smi, p_smi, ts_mol._mol, r_mol._mol, p_mol._mol
 
 input_smiles_path = sys.argv[1]
 output_file_name = sys.argv[2]
 n_jobs = int(sys.argv[3])
 ts_id_to_xyz_path = sys.argv[4]
-
-submit_dir = os.getcwd()
-
-# input_smiles_path = "/home/gridsan/groups/RMG/Projects/Hao-Wei-Oscar-Yunsie/production_run/HAbs/inputs/TS_sep1a_all/wb97xd_and_xtb_opted_ts_combo_results_hashed_sep1a_ts_input.csv"
-# n_jobs = 8
-# output_file_name = "test"
-# ts_id_to_xyz_path = "/home/gridsan/groups/RMG/Projects/Hao-Wei-Oscar-Yunsie/production_run/HAbs/inputs/TS_sep1a_all/wb97xd_and_xtb_opted_ts_combo_results_hashed_sep1a_ts_dft_xyz.pkl"
 
 df = pd.read_csv(input_smiles_path)
 ts_id_to_smi = dict(zip(df.id, df.rxn_smiles))
@@ -56,12 +74,12 @@ ts_ids = list(df.id)
 with open(ts_id_to_xyz_path, "rb") as f:
     ts_id_to_xyz = pkl.load(f)
 
-out = Parallel(n_jobs=n_jobs, backend="multiprocessing", verbose=5)(delayed(parser)(ts_id, submit_dir) for ts_id in ts_ids)
+out = Parallel(n_jobs=n_jobs, backend="multiprocessing", verbose=5)(delayed(parser)(ts_id) for ts_id in ts_ids)
 out = [x for x in out if x is not None]
-csv_file = os.path.join(submit_dir, f'{output_file_name}.csv')
-ts_writer = Chem.rdmolfiles.SDWriter(os.path.join(submit_dir, f'{output_file_name}_ts.sdf'))
-r_writer = Chem.rdmolfiles.SDWriter(os.path.join(submit_dir, f'{output_file_name}_reactants.sdf'))
-p_writer = Chem.rdmolfiles.SDWriter(os.path.join(submit_dir, f'{output_file_name}_products.sdf'))
+csv_file = os.path.join(f'{output_file_name}.csv')
+ts_writer = Chem.rdmolfiles.SDWriter(os.path.join(f'{output_file_name}_ts.sdf'))
+r_writer = Chem.rdmolfiles.SDWriter(os.path.join(f'{output_file_name}_reactants.sdf'))
+p_writer = Chem.rdmolfiles.SDWriter(os.path.join(f'{output_file_name}_products.sdf'))
 
 header = ['id', 'rsmi', 'psmi']
 
